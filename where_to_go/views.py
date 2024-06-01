a4ed15f8d566e57be6f447a36209d110470c5c7c
Sterrrrest <sterrrrest@gmail.com>
2024-06-01 15:17:52 +0300

    Moved data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from places.models import Place, Image
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    return render(request, 'index.html')
+    value = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "static/places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "static/places/roofs24.json"
+                }
+            }
+        ]
+    }
+    data = {'value': value}
+    return render(request, 'index.html', context=data)
