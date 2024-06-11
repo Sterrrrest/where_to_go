60647933abf0bd43207e51bb87f94ac0fc59984b
Sterrrrest <sterrrrest@gmail.com>
2024-06-11 18:58:05 +0300

    Created Img Inline adding

--- places/admin.py
+++ places/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
-admin.site.register(Place)
+class Imageinline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [Imageinline]
+    list_display = ('title',)
+
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_filter = ('place',)
