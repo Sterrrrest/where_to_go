ab376ad74744fe201a2ce1e85c671c424400628c
Sterrrrest <sterrrrest@gmail.com>
2024-06-12 12:55:49 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
