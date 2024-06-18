f311f79b57c027838dc5114b0219c8cf6adcb459
Sterrrrest <sterrrrest@gmail.com>
2024-06-18 15:42:01 +0300

    Fixed verbose name in models

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField('Название', max_length=50)
     description_short = models.TextField('Короткое описание')
     description_long = HTMLField('Длинное описание')
-    lng = models.FloatField('longitude')
-    lat = models.FloatField('latitude')
+    lng = models.FloatField(verbose_name='Длина')
+    lat = models.FloatField(verbose_name='Широта')
 
     geo_title = models.CharField('Название на карте', max_length=50, blank=True)
     placeId = models.CharField('ID', max_length=50, blank=True)
@@ -17,7 +17,7 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, verbose_name='Name', related_name='place_images', on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, verbose_name='Название', related_name='place_images', on_delete=models.CASCADE)
     image = models.ImageField(verbose_name='Картинка', upload_to='media', blank=True, null=True)
     position = models.PositiveIntegerField(verbose_name='Позиция', default=0, db_index=True)
 
