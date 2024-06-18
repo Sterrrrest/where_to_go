e83cca0234f5c46abf10a1d45715a249cb9257ce
Sterrrrest <sterrrrest@gmail.com>
2024-06-18 15:38:54 +0300

    Sorted libs in urls

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.urls import path, include
+
+from where_to_go import views
 
 
 urlpatterns = [
