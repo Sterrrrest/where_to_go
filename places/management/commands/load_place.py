8249bcffc62257aa9a12e2dde73d1070aed58648
Sterrrrest <sterrrrest@gmail.com>
2024-06-26 16:30:45 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -22,19 +22,19 @@ class Command(BaseCommand):
         url = options['url']
         response = requests.get(url)
         new_place = response.json()
-        Place.objects.get_or_create(title=new_place['title'],
-                                    short_description=new_place['description_short'],
-                                    long_description=new_place['description_long'],
-                                    lng=new_place['coordinates']['lng'],
-                                    lat=new_place['coordinates']['lat'],
-                                    geo_title=new_place['title'])
-        place = Place.objects.get(title=new_place['title'])
+        place = Place.objects.update_or_create(title=new_place['title'],
+                                               defaults={
+                                                    'short_description': new_place['description_short'],
+                                                    'long_description': new_place['description_long'],
+                                                    'lng': new_place['coordinates']['lng'],
+                                                    'lat': new_place['coordinates']['lat'],
+                                                    # 'geo_title': new_place['title'],
+                                       })
+
 
         for img_num, img in enumerate(new_place['imgs']):
-
-            Image.objects.get_or_create(place=place, position=img_num)
-            image = Image.objects.get(place=place, position=img_num)
-
+            Image.objects.get_or_create(place=place[0], position=img_num)
+            image = Image.objects.get(place=place[0], position=img_num)
             picture = BytesIO(urlopen(img).read())
             image.image.save(f'{img_num}.jpg', picture, save=True)
 
