9e7b980192b35efa425531b0be9cb436962a7d3b
Sterrrrest <sterrrrest@gmail.com>
2024-06-26 12:58:11 +0300

    Changed var names

--- places/views.py
+++ places/views.py
@@ -1,2 +1,48 @@
 from django.shortcuts import render
+from places.models import Place, Image
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.urls import reverse
 
+
+def index(request):
+    places = Place.objects.all()
+    feature = []
+    for place in places:
+        feature.append({
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [place.lng, place.lat]
+                    },
+                    'properties': {
+                        'title': place.title,
+                        'placeId': place.id,
+                        'detailsUrl': reverse('places', kwargs={'id': place.id})
+                    }
+                },)
+
+    place_data = {
+        'type': 'FeatureCollection',
+        'features': feature
+    }
+
+    return render(request, 'index.html', context={'place_data': place_data})
+
+
+def get_place(request, id):
+    place = get_object_or_404(Place, id=id)
+    images = Image.objects.filter(place_id=id)
+
+    point = {
+        'title': place.title,
+        'imgs': [pic.image.url for pic in images],
+        'short_description': place.short_description,
+        'long_description': place.long_description,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat
+        }
+    }
+
+    return JsonResponse(point, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
