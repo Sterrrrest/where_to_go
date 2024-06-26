8d96b3b78c18c0a70be9d769d89d783e606e67c8
Sterrrrest <sterrrrest@gmail.com>
2024-06-26 18:26:38 +0300

    Added errors checking

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,43 +1,39 @@
 import requests
-import argparse
+import time
+
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
-from urllib.request import urlopen
-
-from django.core.files import File
-from PIL import Image as Im
-from io import BytesIO
-from django.core.files.base import ContentFile
-
-from django.core.files.temp import NamedTemporaryFile
-from django.core.files.images import ImageFile
 
 from places.models import Place, Image
-
-from PIL import Image as Im
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         url = options['url']
-        response = requests.get(url)
-        new_place = response.json()
-        place = Place.objects.update_or_create(title=new_place['title'],
-                                               defaults={
-                                                    'short_description': new_place['description_short'],
-                                                    'long_description': new_place['description_long'],
-                                                    'lng': new_place['coordinates']['lng'],
-                                                    'lat': new_place['coordinates']['lat'],
-                                                    # 'geo_title': new_place['title'],
-                                       })
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            new_place = response.json()
+            place = Place.objects.update_or_create(title=new_place['title'],
+                                                   defaults={
+                                                        'short_description': new_place['description_short'],
+                                                        'long_description': new_place['description_long'],
+                                                        'lng': new_place['coordinates']['lng'],
+                                                        'lat': new_place['coordinates']['lat'],
+                                                        # 'geo_title': new_place['title'],
+                                           })
 
-        for img_num, img in enumerate(new_place['imgs']):
-            pic_response = requests.get(img).content
-            image = ContentFile(pic_response, name=f'{img_num}.jpg')
-            Image.objects.create(place=place[0], image=image, position=img_num)
-
-
+            for img_num, img in enumerate(new_place['imgs']):
+                pic_response = requests.get(img)
+                pic_response.raise_for_status()
+                image = ContentFile(pic_response.content, name=f'{img_num}.jpg')
+                Image.objects.create(place=place[0], image=image, position=img_num)
+        except requests.exceptions.HTTPError:
+            print('HTTPError')
+        except requests.exceptions.ConnectionError:
+            time.sleep(30)
+            
     def add_arguments(self, parser):
         parser.add_argument(
             '--url',
