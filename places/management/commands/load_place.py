e0e7824df7939fa8eeb8f178ea955540ee4e90f2
Sterrrrest <sterrrrest@gmail.com>
2024-06-28 12:59:06 +0300

    Fixed load_place code

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
             response = requests.get(url)
             response.raise_for_status()
             new_place = response.json()
-            place = Place.objects.update_or_create(title=new_place['title'],
+            place, created = Place.objects.update_or_create(title=new_place['title'],
                                                    defaults={
                                                         'short_description': new_place['description_short'],
                                                         'long_description': new_place['description_long'],
@@ -27,17 +27,18 @@ class Command(BaseCommand):
                 pic_response = requests.get(img)
                 pic_response.raise_for_status()
                 image = ContentFile(pic_response.content, name=f'{img_num}.jpg')
-                Image.objects.create(place=place[0], image=image, position=img_num)
+                Image.objects.create(place=place, image=image, position=img_num)
         except requests.exceptions.HTTPError:
             print('HTTPError')
+            pass
         except requests.exceptions.ConnectionError:
-            time.sleep(30)
+            print('ConnectionError')
+            pass
 
     def add_arguments(self, parser):
         parser.add_argument(
             '--url',
             '-u',
-            default=False,
             help='URL нового места'
         )
 
